9cbfce8cd55ccf4e77d7eadef5fb45eb880cc595
Tobiasz12 <tobiasz63@op.pl>
2017-05-23 21:25:19 +0200

    task.26

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Counter = React.createClass({
 	getInitialState: function(){
 		return{
 			counter : 0,
-			
+			timer : 0,		
 		};
 	},
 
@@ -12,61 +12,39 @@ var Counter = React.createClass({
 			
 		});
 	},
-
-	decrement: function() {
+	
+	incrementTimer: function() {
 		this.setState({
-			counter: this.state.counter - 1,
+			timer: this.state.timer + 1,
 			
 		});
 	},
 
-	render: function(){
-		return React.createElement('div',{},
-			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.increment}),
-			React.createElement('span',{},'licznik '+ this.state.counter),
-			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.decrement}),
-		);
-	},
-
-});
-
-var Timer = React.createClass({
-	getInitialState: function() {
-		return {
-			timer : 0
-		};
-	},
-
-	increment: function() {
-		this.setState ({
-			timer: this.state.timer + 1
-		});
-	},
 	decrement: function() {
 		this.setState({
-			timer: this.state.timer - 1
+			counter: this.state.counter - 1,
 		});
 	},
+	
+	decrementTimer: function() {
+		this.setState({
+			timer: this.state.timer - 1,
+		});
+	},	
+
 	render: function(){
 		return React.createElement('div',{},
-			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.increment}),
+			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.increment}),
+			React.createElement('span',{},'licznik '+ this.state.counter),
+			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.decrement}),
+			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.incrementTimer}),
 			React.createElement('span',{},'licznik '+ this.state.timer),
-			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.decrement}),
+			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.decrementTimer}),
 		);
-	}
+	},
 
 });
 
-var App = React.createClass({ 
-	render: function() {
-		return ( 
-			React.createElement('div', {}, 
-			React.createElement(Counter), 
-			React.createElement(Timer) ) 
- 		); 
- 	} 
-});
 
-
-var app = React.createElement(App);
+var app = React.createElement(Counter);
 ReactDOM.render(app, document.getElementById('app'));
