fe3fed809dc328476269862e5c2a8351d2af4d3f
Tobiasz12 <tobiasz63@op.pl>
2017-05-24 08:26:55 +0200

    task 2.6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Counter = React.createClass({
 	getInitialState: function(){
 		return{
 			counter : 0,
-			timer : 0,		
+					
 		};
 	},
 
@@ -13,38 +13,25 @@ var Counter = React.createClass({
 		});
 	},
 	
-	incrementTimer: function() {
-		this.setState({
-			timer: this.state.timer + 1,
-			
-		});
-	},
-
 	decrement: function() {
 		this.setState({
 			counter: this.state.counter - 1,
 		});
 	},
 	
-	decrementTimer: function() {
-		this.setState({
-			timer: this.state.timer - 1,
-		});
-	},	
-
 	render: function(){
 		return React.createElement('div',{},
 			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.increment}),
 			React.createElement('span',{},'licznik '+ this.state.counter),
 			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.decrement}),
-			React.createElement('button',{className: "fa fa-arrow-right",onClick: this.incrementTimer}),
-			React.createElement('span',{},'licznik '+ this.state.timer),
-			React.createElement('button',{className: "fa fa-arrow-left",onClick: this.decrementTimer}),
 		);
 	},
 
 });
 
+var app = React.createElement('div', {},
+React.createElement(Counter),
+React.createElement(Counter),
+React.createElement(Counter),);
 
-var app = React.createElement(Counter);
 ReactDOM.render(app, document.getElementById('app'));
